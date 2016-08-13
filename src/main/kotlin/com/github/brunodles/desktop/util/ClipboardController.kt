7afc621fd8a0b680fac3aab5a1f15b576fa407cd
Bruno de Lima <dlimaun@gmail.com>
2016-08-13 15:44:12 -0300

    Split Clipboard into two files one for helper functions and another for the controller

---
+++ src/main/kotlin/com/github/brunodles/desktop/util/ClipboardController.kt
@@ -0,0 +1,48 @@
+package com.github.brunodles.desktop.util
+
+import java.awt.Toolkit
+import java.awt.datatransfer.Clipboard
+import java.awt.datatransfer.ClipboardOwner
+import java.awt.datatransfer.FlavorListener
+import java.awt.datatransfer.Transferable
+
+/**
+ * Created by bruno on 13/08/16.
+ */
+class ClipboardController : ClipboardOwner {
+
+    val clipboard = Toolkit.getDefaultToolkit().systemClipboard
+    var clipValue = ""
+    val thread = Thread(Runnable {
+        while (!Thread.interrupted()) {
+            val clip = clipboard.getClipboardText()
+            onNewValue(clip)
+            Thread.sleep(1000)
+
+        }
+    });
+    val flavorListener = FlavorListener { onNewValue(clipboard.getClipboardText()) }
+
+    fun start() {
+        clipboard.addFlavorListener(flavorListener)
+        thread.start()
+    }
+
+    fun stop() {
+        clipboard.removeFlavorListener(flavorListener)
+        thread.interrupt()
+        thread.join()
+    }
+
+    private fun onNewValue(clip: String) {
+        if (clip != clipValue) {
+            clipValue = clip
+            println(clip)
+        }
+    }
+
+    override fun lostOwnership(clipboard: Clipboard?, contents: Transferable?) {
+        println("lostOwnership")
+    }
+
+}
