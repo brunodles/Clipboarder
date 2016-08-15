6edcf9994e5ceabb35ad5563efaba2d77414e34b
Bruno de Lima <dlimaun@gmail.com>
2016-08-14 21:08:51 -0300

    Update FileDao to add list function

--- src/main/kotlin/com/github/brunodles/clipboarder/FileDao.kt
+++ src/main/kotlin/com/github/brunodles/clipboarder/FileDao.kt
@@ -1,6 +1,7 @@
 package com.github.brunodles.clipboarder
 
 import java.io.File
+import java.util.*
 
 /**
  * Created by bruno on 14/08/16.
@@ -8,10 +9,19 @@ import java.io.File
 class FileDao(val dirPath : String) {
 
     fun save(name: String, txt: String) {
-        val dir = File("$dirPath")
+        val dir = File(dirPath)
         if (!dir.exists()) dir.mkdir()
         File("$dirPath/$name").printWriter().use { out ->
             out.print(txt)
         }
+    }
+
+    fun list(): HashMap<String, String> {
+        val list = File(dirPath).list()
+        val result = HashMap<String, String>()
+        list.forEach { f ->
+            result.put(f, File("$dirPath/$f").readLines().joinToString(separator = " "))
+        }
+        return result
     }
 }
