5a98ddb3768538f05d62a7a58cca416e07034e7e
Bruno de Lima <dlimaun@gmail.com>
2016-08-14 21:10:27 -0300

    Update Main to add InputReader

--- src/main/kotlin/com/github/brunodles/clipboarder/Main.kt
+++ src/main/kotlin/com/github/brunodles/clipboarder/Main.kt
@@ -1,6 +1,7 @@
 package com.github.brunodles.clipboarder
 
 import com.github.brunodles.desktop.util.ClipboardController
+import com.github.brunodles.desktop.util.InputReader
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -12,9 +13,14 @@ fun main(args: Array<String>) {
     val fileDao = FileDao(".clipboard")
     val controller = ClipboardController() { txt -> fileDao.save(System.currentTimeMillis().toString(), txt) }
     controller.start()
+
     val format = SimpleDateFormat("HH:mm:ss")
     println("Start ${format.format(Date())}")
-    Thread.sleep(15000)
+
+    InputReader("exit")
+            .add(Regex("list"), { fileDao.list().forEach { name, s -> println("$name -> $s") } })
+            .start()
+
     println("Stop ${format.format(Date())}")
     controller.stop()
 }
